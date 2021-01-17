11593dff4cb83940ac413215fd2cdc34e796a9c7
Memet <meavci@gmail.com>
2021-01-17 17:01:53 -0500

    connect database

---
+++ Blog-backend/app/controllers/blogpost.controler.js
@@ -0,0 +1,19 @@
+const db = require("../models");
+const BlogPost = db.blogposts;
+
+// Retrieve all BlogPost from the database.
+exports.findAll = (req, res) => {
+  const title = req.query.title;
+  var condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {};
+
+  BlogPost.find(condition)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving todo items."
+      });
+    });
+};
--- Blog-backend/server.js
+++ Blog-backend/server.js
@@ -2,27 +2,38 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const app=express();
+const app = express();
+
 var corsOptions = {
   origin: "http://127.0.0.1:3000"
 };
-app.use(cors(corsOptions))
-// parse requests of content-type - application/json
+
+app.use(cors(corsOptions));
+
 app.use(bodyParser.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Database Connection
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Blog Post  application." });
+  res.json({ message: "Welcome to BlogPost application." });
 });
-
-app.get("/api/posts", (req, res) => {
-  res.json({ message: "List of posts" });
-});
-
-
+require("./app/routes/blogpost.routes")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
